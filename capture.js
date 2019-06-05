fb522ce0dbc5fa710bdb61389518cde9cf6ce051
iwant12cats <hi@alexpetercharles.ch>
2019-06-05 09:25:26 +0200

    leap initializing events

--- capture.js
+++ capture.js
@@ -1,15 +1,41 @@
 /* eslint-disable no-console */
 
 const Readline = require('readline');
+const Leap = require('leapjs');
+
+let leapConnected = false;
 
 // setup key listen
 Readline.emitKeypressEvents(process.stdin);
+// TODO: this throws error, might be worth to change input method to redStream
 process.stdin.setRawMode(true);
 
-console.log('Press Space to capture Frame');
+// controller is main leap motion main interface
+const Controller = new Leap.Controller();
+// start listener
+Controller.connect();
 
-// on keypress
+// listener event
+
+// leap connected to websocket!
+Controller.on('connect', () => {
+  console.log('leap connected to websocket!');
+});
+
+// leap started sending data!
+Controller.on('deviceStreaming', () => {
+  console.log('leap is sending data!');
+  console.log('----------------------------');
+  console.log('press space to capture frame data!');
+  leapConnected = true;
+});
+
+
+// setting up keypress Event
 process.stdin.on('keypress', (str, key) => {
+  // only when leap is connected
+  if (!leapConnected) return;
+
   // if ctrl + c stop else output data
   if (key.ctrl && key.name === 'c') {
     console.log('exiting...');
